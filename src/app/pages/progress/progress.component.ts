ffe5822c09a13dfabf147c94f83dafaccde1d8e5
mabvmex <mabvmex@gmail.com>
2022-03-09 23:48:13 -0600

    componente progressBar + input

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
@@ -6,11 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './progress.component.css'
 ]
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+  progreso: number = 5;
 
-  constructor() { }
+  get getPorcentaje() {
+    return `${this.progreso}%`
+  }
 
-  ngOnInit(): void {
+
+  cambiarValor(valor: number) {
+
+    if (this.progreso >= 100 && valor >=0){
+    this.progreso = 100;
+    return;
+    }
+
+    if (this.progreso <= 0 && valor < 0){
+    this.progreso = 0;
+    return;
+    }
+   
+  this.progreso = this.progreso + valor;  
   }
 
 }
+
